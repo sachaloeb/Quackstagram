80c922e8a4b676351fb5df71dbf35de60a482a6e
sachaloeb <144175746+sachaloeb@users.noreply.github.com>
2024-05-18 00:16:17 +0200

    new updates

--- src/DataStorage/SQLDBConnection.java
+++ src/DataStorage/SQLDBConnection.java
@@ -5,14 +5,14 @@ import java.sql.*;
 
 
 public class SQLDBConnection {
-    public static void main(String[] args) throws ClassNotFoundException{
+    public static void main(String[] args){
         try{
             Connection myCon =DriverManager.getConnection("jdbc:mysql://localhost:3306/QuackDB", "root", "");
 
             Statement stmt = myCon.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM Users"); 
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Images"); 
             while (rs.next()){
-                System.out.print(rs.getString("username"));
+                System.out.println(rs.getString("username"));
             }
         }catch(SQLException e){
             e.printStackTrace();
