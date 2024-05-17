bcbfc6c06f187e47a1fb436dad6552253acf1b4d
sachaloeb <144175746+sachaloeb@users.noreply.github.com>
2024-05-17 15:25:31 +0200

    sql connector works

--- src/DataStorage/SQLDBConnection.java
+++ src/DataStorage/SQLDBConnection.java
@@ -5,11 +5,18 @@ import java.sql.*;
 
 
 public class SQLDBConnection {
-    public static void main(String[] args) {
-        Class.forName(com.mysql.jdbc.Driver);
-        Connection myCon =DriverManager.getConnection("jdbc:mysql://localhost:3306/TransitorDB", "root", "");
+    public static void main(String[] args) throws ClassNotFoundException{
+        try{
+            Connection myCon =DriverManager.getConnection("jdbc:mysql://localhost:3306/QuackDB", "root", "");
 
-        Statement stmt = myCon.createStatement();
-        ResultSet rs = stmt.executeQuery("SELECT * FROM Agency");
+            Statement stmt = myCon.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Users"); 
+            while (rs.next()){
+                System.out.print(rs.getString("username"));
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        
     }
 }
