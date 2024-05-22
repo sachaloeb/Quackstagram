64cedb4de4188c4570bd911e4423900deaba7ca3
sachaloeb <144175746+sachaloeb@users.noreply.github.com>
2024-05-22 12:06:32 +0200

    changes in DB

--- SQLDB/Schema.sql
+++ SQLDB/Schema.sql
@@ -18,7 +18,7 @@ CREATE TABLE `Users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
-INSERT INTO `Users` VALUES ('1','6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b','1'),('dani','fe675fe7aaee830b6fed09b64e034f84dcbdaeb429d9cccd4ebb90e15af8dd71','im chinese'),('Mystar','e7cf3ef4f17c3999a94f2c6f612e8a888e5b1026878e4e19398b23bd38ec221a',''),('sacha','03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4','its me'),('test','9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08','test'),('test1','1b4f0e9851971998e732078544c96b36c3d01cedf7caa332359d6f1d83567014','imatest'),('test2','e7cf3ef4f17c3999a94f2c6f612e8a888e5b1026878e4e19398b23bd38ec221a',''),('Xylo','e7cf3ef4f17c3999a94f2c6f612e8a888e5b1026878e4e19398b23bd38ec221a',''),('Zara','e7cf3ef4f17c3999a94f2c6f612e8a888e5b1026878e4e19398b23bd38ec221a','');
+INSERT INTO `Users` VALUES ('1','6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b','1'),('dani','fe675fe7aaee830b6fed09b64e034f84dcbdaeb429d9cccd4ebb90e15af8dd71','im chinese'),('maya','ec4f2dbb3b140095550c9afbbb69b5d6fd9e814b9da82fad0b34e9fcbe56f1cb','t'),('Mystar','e7cf3ef4f17c3999a94f2c6f612e8a888e5b1026878e4e19398b23bd38ec221a',''),('regis','07123e1f482356c415f684407a3b8723e10b2cbbc0b8fcd6282c49d37c9c1abc','im no one'),('sacha','03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4','its me'),('test','9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08','test'),('test1','1b4f0e9851971998e732078544c96b36c3d01cedf7caa332359d6f1d83567014','imatest'),('test2','e7cf3ef4f17c3999a94f2c6f612e8a888e5b1026878e4e19398b23bd38ec221a',''),('tom','961b6dd3ede3cb8ecbaacbd68de040cd78eb2ed5889130cceb4c49268ea4d506','ouais'),('Xylo','e7cf3ef4f17c3999a94f2c6f612e8a888e5b1026878e4e19398b23bd38ec221a',''),('Zara','e7cf3ef4f17c3999a94f2c6f612e8a888e5b1026878e4e19398b23bd38ec221a','');
 
 
 
@@ -35,8 +35,7 @@ CREATE TABLE `Images` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
-INSERT INTO `Images` VALUES ('Mystar_1','Mystar','Cookies gone?','2023-12-17 19:26:50',1),('Mystar_2','Mystar','In my soup a fly is.','2023-12-17 19:27:24',1),('Xylo_2','Xylo','Jedi mind trick failed.','2023-12-17 19:23:14',1),('Zara_1','Zara','Lost my map I have. Oops.','2023-12-17 19:24:31',1),('Zara_2','Zara','Yoga with Yoda','2023-12-17 19:25:03',1);
-
+INSERT INTO `Images` VALUES ('maya_1','maya','Enter a caption','2024-05-21 23:11:23',1),('maya_2','maya','Enter a caption','2024-05-21 23:19:34',1),('Mystar_1','Mystar','Cookies gone?','2023-12-17 19:26:50',3),('Mystar_2','Mystar','In my soup a fly is.','2023-12-17 19:27:24',1),('sacha_1','sacha','Enter a caption','2024-05-21 22:24:50',0),('Xylo_2','Xylo','Jedi mind trick failed.','2023-12-17 19:23:14',1),('Xylo_3','Xylo','Enter a caption','2024-05-21 23:25:23',0),('Zara_1','Zara','Lost my map I have. Oops.','2023-12-17 19:24:31',2),('Zara_2','Zara','Yoga with Yoda','2023-12-17 19:25:03',3);
 
 
 
@@ -50,7 +49,7 @@ CREATE TABLE `Follows` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
-INSERT INTO `Follows` VALUES ('sacha','1'),('test','Mystar'),('sacha','Xylo');
+INSERT INTO `Follows` VALUES ('sacha','1'),('Xylo','maya'),('maya','Mystar'),('sacha','Mystar'),('test','Mystar'),('Zara','Mystar'),('sacha','Xylo'),('maya','Zara'),('Mystar','Zara'),('Xylo','Zara');
 
 
 
@@ -69,7 +68,7 @@ CREATE TABLE `Likes` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
-INSERT INTO `Likes` VALUES ('Xylo_2','sacha'),('Zara_1','sacha'),('Mystar_1','test'),('Mystar_2','test'),('Zara_2','test');
+INSERT INTO `Likes` VALUES ('Mystar_1','maya'),('Zara_2','maya'),('Mystar_1','Mystar'),('Xylo_2','sacha'),('Zara_1','sacha'),('Mystar_1','test'),('Mystar_2','test'),('Zara_2','test'),('maya_1','Xylo'),('maya_2','Xylo'),('Zara_1','Xylo'),('Zara_2','Xylo');
 
 
 
@@ -89,10 +88,10 @@ CREATE TABLE `Notifications` (
   CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`notif_receiver`) REFERENCES `Users` (`username`),
   CONSTRAINT `notifications_ibfk_2` FOREIGN KEY (`concerned_user`) REFERENCES `Users` (`username`),
   CONSTRAINT `notifications_ibfk_3` FOREIGN KEY (`liked_picture`) REFERENCES `Images` (`imageID`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
-INSERT INTO `Notifications` VALUES (1,'Mystar','test','Mystar_1','like','2024-05-08 18:14:11'),(2,'Zara','test','Zara_2','like','2024-05-08 18:14:20'),(3,'Mystar','test','Mystar_2','like','2024-05-08 18:14:35'),(4,'Mystar','test',NULL,'follow','2024-05-08 18:14:49'),(5,'Zara','sacha','Zara_1','like','2024-05-15 15:50:52'),(6,'1','sacha',NULL,'follow','2024-05-15 15:51:49'),(7,'Xylo','sacha',NULL,'follow','2024-05-15 15:52:41'),(8,'Xylo','sacha','Xylo_2','like','2024-05-15 15:52:52');
+INSERT INTO `Notifications` VALUES (1,'Mystar','test','Mystar_1','like','2024-05-08 18:14:11'),(2,'Zara','test','Zara_2','like','2024-05-08 18:14:20'),(3,'Mystar','test','Mystar_2','like','2024-05-08 18:14:35'),(4,'Mystar','test',NULL,'follow','2024-05-08 18:14:49'),(5,'Zara','sacha','Zara_1','like','2024-05-15 15:50:52'),(6,'1','sacha',NULL,'follow','2024-05-15 15:51:49'),(7,'Xylo','sacha',NULL,'follow','2024-05-15 15:52:41'),(8,'Xylo','sacha','Xylo_2','like','2024-05-15 15:52:52'),(9,'Zara','Xylo',NULL,'follow','2024-05-21 19:16:14'),(10,'Mystar','Zara',NULL,'follow','2024-05-21 19:22:05'),(11,'Mystar','Zara','Mystar_1','like','2024-05-21 19:22:40'),(12,'Mystar','Zara','Mystar_1','like','2024-05-21 19:22:41'),(13,'Mystar','sacha',NULL,'follow','2024-05-21 22:25:36'),(14,'Mystar','sacha','Mystar_1','like','2024-05-21 22:26:20'),(15,'Mystar','sacha','Mystar_1','like','2024-05-21 22:26:21'),(16,'Mystar','sacha','Mystar_1','like','2024-05-21 22:26:37'),(17,'Mystar','sacha','Mystar_2','like','2024-05-21 22:26:51'),(18,'Xylo','sacha','Xylo_2','like','2024-05-21 22:27:03'),(19,'Zara','Mystar',NULL,'follow','2024-05-21 22:45:23'),(20,'Zara','Mystar','Zara_1','like','2024-05-21 22:45:31'),(21,'Zara','Mystar','Zara_2','like','2024-05-21 22:45:37'),(22,'Mystar','Mystar','Mystar_1','like','2024-05-21 22:46:12'),(23,'Mystar','Mystar','Mystar_1','like','2024-05-21 22:46:14'),(24,'Zara','maya',NULL,'follow','2024-05-21 23:01:29'),(25,'Mystar','maya',NULL,'follow','2024-05-21 23:01:36'),(26,'Mystar','maya','Mystar_2','like','2024-05-21 23:01:39'),(27,'Mystar','maya','Mystar_1','like','2024-05-21 23:01:40'),(28,'Zara','maya','Zara_1','like','2024-05-21 23:01:42'),(29,'Zara','maya','Zara_1','like','2024-05-21 23:01:52'),(30,'Zara','maya','Zara_2','like','2024-05-21 23:20:07'),(31,'Mystar','maya','Mystar_1','like','2024-05-21 23:20:14'),(32,'Mystar','maya','Mystar_1','like','2024-05-21 23:20:38'),(33,'maya','Xylo',NULL,'follow','2024-05-21 23:22:48'),(34,'maya','Xylo','maya_1','like','2024-05-21 23:22:53'),(35,'maya','Xylo','maya_2','like','2024-05-21 23:22:54'),(36,'Zara','Xylo','Zara_2','like','2024-05-21 23:22:56'),(37,'Zara','Xylo','Zara_1','like','2024-05-21 23:22:58'),(38,'maya','Xylo','maya_1','like','2024-05-21 23:37:43');
 
 
 
@@ -124,7 +123,7 @@ CREATE TABLE `hashtags` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
-INSERT INTO `hashtags` VALUES ('#cool','Mystar_1'),('#funny','Mystar_2'),('#amazing','Xylo_2'),('#cool','Zara_1'),('#yoga','Zara_2');
+INSERT INTO `hashtags` VALUES ('search','maya_1'),('mars','maya_2'),('cool','Mystar_1'),('funny','Mystar_1'),('funny','Mystar_2'),('amazing','Xylo_2'),('sword','Xylo_2'),('elections','Xylo_3'),('france','Xylo_3'),('politics','Xylo_3'),('zemmour','Xylo_3'),('Amazing','Zara_1'),('cool','Zara_1'),('yoga','Zara_2');
 
 
 
